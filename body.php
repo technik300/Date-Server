7936badd58ea82a721253cbe4fdcd2a88c8eb142
technik300 <40206044+technik300@users.noreply.github.com>
2022-10-19 13:50:40 +0300

    Update body.php

--- body.php
+++ body.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ru">
 	<head>
-		<title>Time server</title>
+		<title>JSON server</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
       <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=5.0, user-scalable=yes">
 		<link rel="stylesheet" type="text/css" href="style.css">
@@ -15,8 +15,10 @@
 			   <input class="test-date" type="date" name="date" id="date" value="" onchange="formatDate(this.value)"/>
 				<output id="result" value=""></output>
 			</form>
-			<button class="btn get" type="button" onclick="submitForm('get')" >GET</button>
-			<button class="btn set" type="button" onclick="submitForm('set')" >SET</button>
+			<button class="btn get" type="button" onclick="submitForm('get_get')" >GET GET</button>
+			<button class="btn set" type="button" onclick="submitForm('set_get')" >SET GET</button>			
+			<button class="btn get1" type="button" onclick="submitForm('get_post')" >GET POST</button>
+			<button class="btn set1" type="button" onclick="submitForm('set_post')" >SET POST</button>
 		<input type="checkbox" id="help" name="help" class="hidden"/>	
 			<div class="help">				
 				<?php include('help.php');?>
@@ -36,19 +38,51 @@
 				document.querySelector('#result').value = d.split('-').reverse().join('-');
 			}
 			async function submitForm(type){
-				if(type == 'get' ){
+				if(type == 'get_get' ){
 					document.querySelector('.test-date').value = '';
 					document.querySelector('#result').value = '';
 					let response = await fetch('/index.php?date=', { method: 'GET'});
 					document.querySelector('#result').value = await response.text();
 					document.querySelector('.test-date').value = document.querySelector('#result').value.split('-').reverse().join('-');
 
-				}else if(type == 'set'){
+				}else if(type == 'set_get'){
 					if(document.querySelector('#result').value != ''){
 						let response = await fetch('/index.php?date='+document.querySelector('#result').value, { method: 'GET'});
 						let result = await response.text();
 					}				
-				}
+				}else if(type == 'get_post'){
+					document.querySelector('.test-date').value = '';
+					document.querySelector('#result').value = '';
+					let response = await fetch('/index.php',{ 
+						method: 'POST', 
+						body: new FormData(document.querySelector('#test'))
+					})
+  						.then((response) => {
+
+    						return response.text();
+  						})
+  						.then((data) => {
+							 document.querySelector('#result').value = data;
+							 document.querySelector('.test-date').value = document.querySelector('#result').value.split('-').reverse().join('-');
+  					});
+
+				}else if(type == 'set_post'){
+					if(document.querySelector('#result').value != ''){
+						let response = await fetch('/index.php',{ 
+						method: 'POST', 
+						body: new FormData(document.querySelector('#test'))
+					})
+  						.then((response) => {
+
+    						return response.text();
+  						})
+  						.then((data) => {
+							console.log(data);
+
+  					});
+
+					}
+				}		
 			}
 
 		</script>
